e6d33ed458879d4c695eac09539a7e75f52096e1
Nikita Ivanov <ivnik_15@mail.ru>
2022-12-27 16:21:19 +0300

    fix: preloader and reject

--- scripts/mockComments.js
+++ scripts/mockComments.js
@@ -1,15 +1,19 @@
 (
     function (){
         let commentArea = document.getElementById('table');
-        let loading = "<tr className='tr-odd tr'> <td>Загрузка</td> <td>Загрузка</td> <td>Загрузка</td></tr>";
-        let clear = commentArea.innerHTML;
-        commentArea.innerHTML += loading;
+        let loadingGif = document.getElementById('preloader');
+        commentArea.style.display = "none"
+        loadingGif.style.display = "block"
             fetch('https://jsonplaceholder.typicode.com/comments/1')
                 .then(response => response.json())
-                .then(json => commentArea.innerHTML = clear + "<tr class='tr-odd tr'> <td>" + json.body.split(" ")[3] + "</td> <td>" + json.name + "</td> <td>" + json.body + "</td></tr>");
+                .then(json => commentArea.innerHTML += "<tr class='tr-odd tr'> <td>" + json.body.split(" ")[3] + "</td> <td>" + json.name + "</td> <td>" + json.body + "</td></tr>");
             fetch('https://jsonplaceholder.typicode.com/comments/2')
                 .then(response => response.json())
-                .then(json => commentArea.innerHTML += "<tr class='tr-even tr'> <td>" + json.body.split(" ")[3] + "</td> <td>" + json.name + "</td> <td>" + json.body + "</td></tr>");
+                .then(json => commentArea.innerHTML += "<tr class='tr-even tr'> <td>" + json.body.split(" ")[3] + "</td> <td>" + json.name + "</td> <td>" + json.body + "</td></tr>")
+                .then(json => {
+                    loadingGif.style.display = "none"
+                    commentArea.style.display = "block"
+                }, () => commentArea.innerText = "Ошибка!")
     }
 
 )()
